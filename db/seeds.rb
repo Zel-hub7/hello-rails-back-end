d717a430ca69f1fc3005ffd19789c55ea08947c5
unknown <zelalemyohannes75@gmail.com>
2023-12-19 10:15:33 +0300

    create API end point

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+greetings = [
+  "Hello, World!",
+  "Greetings from Rails!",
+  "Welcome to the API!",
+  "Hola amigos!",
+  "Bonjour tout le monde!"
+]
+
+greetings.each do |greeting|
+    Message.create(content: greeting)
+end
